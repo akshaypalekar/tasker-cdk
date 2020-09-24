52085e86c55b090cf9b9a6a60c152b93fb5553da
Akshay Palekar <apalekar@jahnelgroup.com>
2020-09-24 08:36:53 -0400

    Initial commit

---
+++ bin/tasker-cdk.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { TaskerCdkStack } from '../lib/tasker-cdk-stack';
+
+const app = new cdk.App();
+new TaskerCdkStack(app, 'TaskerCdkStack');
